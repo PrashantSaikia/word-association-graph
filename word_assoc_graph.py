6c9ca2c47f791735bff4c76db831d9ce733bb8d0
Prashant Saikia <39755678+PrashantSaikia@users.noreply.github.com>
2019-01-08 18:38:36 +0800

    Add files via upload

---
+++ word_assoc_graph.py
@@ -0,0 +1,45 @@
+import nltk
+import pandas as pd
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def plot_word_associations(text):
+    nouns_in_text = []
+    is_noun = lambda pos: pos[:2] == 'NN'
+
+    for sent in text.split('.')[:-1]:   
+        tokenized = nltk.word_tokenize(sent)
+        nouns=[word for (word, pos) in nltk.pos_tag(tokenized) if is_noun(pos)]
+        nouns_in_text.append(' '.join([word for word in nouns if not (word=='' or len(word)==1)]))
+
+    nouns_list = []
+
+    for sent in nouns_in_text:
+        temp = sent.split(' ')
+        for word in temp:
+            if word not in nouns_list:
+                nouns_list.append(word)
+
+    df = pd.DataFrame(np.zeros(shape=(len(nouns_list),2)), columns=['Nouns', 'Verbs & Adjectives'])
+    df['Nouns'] = nouns_list
+
+    is_adjective_or_verb = lambda pos: pos[:2]=='JJ' or pos[:2]=='VB'
+    for sent in text.split('.'):
+        for noun in nouns_list:
+            if noun in sent:
+                tokenized = nltk.word_tokenize(sent)
+                adjectives_or_verbs = [word for (word, pos) in nltk.pos_tag(tokenized) if is_adjective_or_verb(pos)]
+                ind = df[df['Nouns']==noun].index[0]
+                df['Verbs & Adjectives'][ind]=adjectives_or_verbs
+
+    fig = plt.figure(figsize=(30,20))
+    G = nx.Graph()
+
+    for i in range(len(df)):
+        G.add_node(df['Nouns'][i])
+        for word in df['Verbs & Adjectives'][i]:
+            G.add_edges_from([(df['Nouns'][i], word)])
+
+    pos = nx.kamada_kawai_layout(G)
+    nx.draw(G, with_labels=True, font_size=20) #font_weight='bold', 
